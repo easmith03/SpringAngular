6c760a8ca61305035c975fe9493dd40f09347905
Eric Smith <easmith03@comcast.net>
2018-01-27 15:52:25 -0500

    configure postgresql db.

---
+++ api/src/main/java/hello/HelloController.java
@@ -0,0 +1,16 @@
+package hello;
+
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RestController
+@RequestMapping("/hello")
+public class HelloController {
+
+    @GetMapping()
+    public String index() {
+        return "Greetings from Spring Boot!";
+    }
+
+}
